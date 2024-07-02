5eaf487dffbf7b5d45c990664b9779590a0cac18
kahkashan25SH <ahmedalikahkashan@gmail.com>
2024-07-03 00:34:23 +0500

    Adding Word Counter Project

---
+++ index.ts
@@ -0,0 +1,24 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+import chalk from "chalk";
+import { log } from "console";
+
+console.log(chalk.green("\n\tWelcome to the word count program!\t\n"));
+
+console.log("=".repeat(60));
+
+const answers = await inquirer.prompt([
+     {
+       type: "sentence",
+       name: "input",
+       message: "Enter your sentence to count the words:",
+     }
+]);
+
+//const words = answers.Sentence.trim().split(" ");
+
+let words=answers.input.trim().split(' ')
+console.log(words);
+console.log(`your words lenth is ${words.length}`)
+
